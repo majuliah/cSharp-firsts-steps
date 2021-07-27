c4033306fa44a401fbfa45469d43b62387a170a0
Majulia <majulia_carvalho@yahoo.com.br>
2021-07-27 20:00:17 -0300

    adding new exercices

---
+++ manzano-exercices/structure-algorithms/ConverteRealParaDolar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConverteRealParaDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double real, dolar, conversaoRealParaDolar;
+            Console.WriteLine("Entre com o valor em reais para ser convertido para dólar");
+            real = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            dolar = 5.20;
+
+            conversaoRealParaDolar = real / dolar;
+
+            Console.WriteLine("Você pode comprar " + conversaoRealParaDolar.ToString("F2", CultureInfo.InvariantCulture) + "$ dólares");
+        }
+    }
+}
