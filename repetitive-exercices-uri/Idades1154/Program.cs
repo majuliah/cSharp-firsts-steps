cca54527440ae39e33cd2f812ae83177d872b6bc
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-23 01:24:22 -0300

    fim da estrutura while

---
+++ repetitive-exercices-uri/Idades1154/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Idades1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media, idade, contador = 0.0, soma = 0.0;
+            
+            Console.WriteLine("Entre com idades");
+
+            idade = int.Parse(Console.ReadLine());
+
+            while (idade > 0)
+            {
+                contador += 1.0;
+                soma += idade;
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            media = soma / contador;
+            Console.WriteLine($"A mpedia das idades é: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ repetitive-exercices-uri/SomaParesConsecutivos1159/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SomaParesConsecutivos1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma, x;
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+                if (x % 2 == 0)
+                {
+                    soma = x + (x + 2) + (x + 4) + (x + 6) + (x + 8);
+                    Console.WriteLine($"A soma é {soma}");
+                }
+                else
+                {
+                    soma = x + 1 + (x + 3) + (x + 5) + (x + 7) + (x + 9);
+                    Console.WriteLine($"A soma é {soma}");
+                }
+
+                Console.WriteLine("Continuar?");
+                x = int.Parse(Console.ReadLine());
+                
+            }
+
+        }
+    }
+}
+
