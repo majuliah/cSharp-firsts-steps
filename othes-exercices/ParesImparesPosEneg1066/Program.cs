2bf4c4cf04f19eb183ba5c9dd6596cc23403f07a
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-24 00:26:16 -0300

    mais exercícios

--- othes-exercices/ParesImparesPosEneg1066/Program.cs
+++ othes-exercices/ParesImparesPosEneg1066/Program.cs
@@ -6,7 +6,36 @@ namespace ParesImparesPosEneg1066
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int valores, par = 0, impar = 0, positivo = 0, negativo = 0;
+
+            for (int i = 0; i <= 4; i++)
+            {
+                valores = int.Parse(Console.ReadLine());
+
+                if (valores % 2 == 0)
+                {
+                    par ++;
+                }
+                else
+                {
+                    impar ++;
+                }
+
+                if (valores >= 0)
+                {
+                    positivo ++;
+                }
+                else
+                {
+                    negativo ++;
+                }
+
+            }
+
+            Console.WriteLine(par + " valor(es) par(es)");
+            Console.WriteLine(impar + " valor(es) impar(es)");
+            Console.WriteLine(positivo + " valor(es) positivo(s)");
+            Console.WriteLine(negativo + " valor(es) negativo(s)");
         }
     }
 }
