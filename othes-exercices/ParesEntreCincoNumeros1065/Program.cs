54460a6902f5c1dee12de496955eed9c16450fc6
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-24 00:09:00 -0300

    exercicios

--- othes-exercices/ParesEntreCincoNumeros1065/Program.cs
+++ othes-exercices/ParesEntreCincoNumeros1065/Program.cs
@@ -6,7 +6,19 @@ namespace ParesEntreCincoNumeros1065
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeros, pares = 0;
+
+            Console.WriteLine("ENtre com 5 valores");
+            for (int i = 0; i <= 4; i++)
+            {
+                numeros = int.Parse(Console.ReadLine());
+                if (numeros % 2 == 0)
+                {
+                    pares += 1;
+                }
+            }
+
+            Console.WriteLine($"{pares} valores pares");
         }
     }
 }
---
+++ othes-exercices/ParesImparesPosEneg1066/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParesImparesPosEneg1066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
