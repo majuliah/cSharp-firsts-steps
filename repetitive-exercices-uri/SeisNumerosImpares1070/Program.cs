01ff429da5faa68bc8556a2ffab539426fc26ad9
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-24 16:26:14 -0300

    exercicio 1070

---
+++ repetitive-exercices-uri/SeisNumerosImpares1070/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SeisNumerosImpares1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                x += 1;
+                Console.WriteLine($"{x}");
+                for (int i = 0; i <= 4; i++)
+                {
+                    x += 2;
+                    Console.WriteLine($"{x}");
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    x += 2;
+                    Console.WriteLine($"{x}");
+                }
+            }
+            
+        }
+    }
+}
