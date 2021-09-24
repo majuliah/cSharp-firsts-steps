746377acc689f25c5ba19ea6d0fa9093dddc364f
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-23 23:37:42 -0300

    início da estrutura for

---
+++ othes-exercices/Tabuada1078/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tabuada1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+            numero = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = i * numero;
+                Console.WriteLine($"{numero} * {i} = {resultado}");
+            }
+        }
+    }
+}
---
+++ othes-exercices/primeiroFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace primeiroFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, x, soma = 0;
+            Console.WriteLine("Entre com um número");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+               x = int.Parse(Console.ReadLine());
+               soma += x;
+            }
+
+            Console.WriteLine($"A soma é {soma}");
+            Console.WriteLine(' ');
+        }
+    }
+}
