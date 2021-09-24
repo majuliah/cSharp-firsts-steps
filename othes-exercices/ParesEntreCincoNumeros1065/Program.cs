ef27b0ae8f52b22baf2e01197e2db75741cee481
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-23 23:59:14 -0300

    exercicios

---
+++ othes-exercices/ParesEntreCincoNumeros1065/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParesEntreCincoNumeros1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
