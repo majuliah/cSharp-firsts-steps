2b914aee64c59548416e2b6a62cc0f90e65e4e7e
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-24 01:02:18 -0300

    exercicios

---
+++ repetitive-exercices-uri/NumerosImpares1067/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumerosImpares1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                for (int i = 1; i < numero; i+=2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i+=2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
