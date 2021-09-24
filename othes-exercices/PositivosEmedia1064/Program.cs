9bc94b166ed53031ddb97c38297ddbc082d594e5
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-23 23:57:26 -0300

    exercicios

---
+++ othes-exercices/PositivosEmedia1064/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace PositivosEmedia1064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeros = 0.0, media, positivos = 0.0, cont = 0.0;
+            Console.WriteLine("Entre com 6 números");
+            for (int i = 0; i <= 5; i++)
+            {
+                numeros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (numeros > 0)
+                {
+                    positivos = positivos + numeros;
+                    cont += 1;
+                }
+            }
+
+            Console.WriteLine($"{cont} valores positivos");
+            media = positivos / cont;
+            Console.WriteLine($"A média é {media.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
