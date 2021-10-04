ffc2b7b6710c29c1d39cd18cb7f2316fd6dcad41
Majulia <majulialcarvalho@gmail.com>
2021-10-04 18:28:19 -0300

    + 1

---
+++ repetitive-exercices-uri/ParOuImpar1074/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ParOuImpar1074
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, numero;
+            Console.WriteLine("Entre com uma quantidade de valores");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine($"Entre com o {i + 1}º numero");
+                number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0 && number > 0)
+                    Console.WriteLine("EVEN POSITIVE");
+                else if (number % 2 == 0 && number < 0)
+                    Console.WriteLine("EVEN NEGATIVE");
+                else if (number % 2 != 0 && number > 0)
+                    Console.WriteLine("ODD POSITIVE");
+                else if (number % 2 != 0 && number < 0)
+                    Console.WriteLine("ODD NEGATIVE");
+                 else
+                     Console.WriteLine("NULL");
+            }
+        }
+    }
+}
