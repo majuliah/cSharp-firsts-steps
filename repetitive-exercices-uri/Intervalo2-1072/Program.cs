83e2b4a1e773741f74856cef211a3b3ceaffa4da
Majulia <majulialcarvalho@gmail.com>
2021-10-03 21:58:25 -0300

    enviando exercicios finais

---
+++ repetitive-exercices-uri/Intervalo2-1072/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Intervalo2_1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, x, valIn = 0, valOut = 0, cont = 0;
+            Console.WriteLine("Entre com um valor inteiro.");
+            valor = int.Parse(Console.ReadLine());
+
+            while (cont < valor)
+            {
+                Console.WriteLine($"Entre com o {cont + 1}º número.");
+                x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    valIn += 1;
+                }
+                else
+                {
+                    valOut += 1;
+                }
+                cont += 1;
+            }
+            Console.WriteLine($"{valIn} in \n {valOut} out");
+            
+        }
+    }
+}
---
+++ repetitive-exercices-uri/SomaDeImparesCosecutivos1071/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SomaDeImparesCosecutivos1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma, min, max;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                min = y;
+                max = x;
+            }
+            else
+            {
+                min = x;
+                max = y;
+            }
+
+            soma = 0;
+
+            for (int i = min + 1; i < max ; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine($"{soma}");
+
+        }
+    }
+}
