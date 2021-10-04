2035fc545414945d7dcb0a05ef649743cfcdd3e8
Majulia <majulialcarvalho@gmail.com>
2021-10-03 22:10:57 -0300

    fim da página 2

---
+++ repetitive-exercices-uri/QuadradoDePares1073/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QuadradoDePares1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            Console.WriteLine("Entre com um valor.");
+            valor = int.Parse(Console.ReadLine());
+
+            if (valor % 2 == 0)
+            {
+                for (int i = 2; i <= valor; i += 2)
+                {
+                    Console.WriteLine($"{i} ^ 2 = {Math.Pow(i, 2)}");
+                }    
+            }
+            else
+            {
+                for (int i = 2; i < valor; i += 2)
+                {
+                    Console.WriteLine($"{i} ^ 2 = {Math.Pow(i, 2)}");
+                }
+            }
+            
+        }
+    }
+}
