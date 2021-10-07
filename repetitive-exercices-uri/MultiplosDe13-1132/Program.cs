c924bc12c299ab3e22127acd15ec69fc2604098a
Majulia <majulialcarvalho@gmail.com>
2021-10-07 01:17:28 -0300

    novos exercicios uri

--- repetitive-exercices-uri/MultiplosDe13-1132/Program.cs
+++ repetitive-exercices-uri/MultiplosDe13-1132/Program.cs
@@ -6,7 +6,34 @@ namespace MultiplosDe13_1132
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero1, numero2, maior, menor, soma = 0;
+            Console.WriteLine("Entre com dois números");
+
+            numero1 = int.Parse(Console.ReadLine());
+            numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                maior = numero1;
+                menor = numero2;
+            }
+            else
+            {
+                menor = numero1;
+                maior = numero2;
+            }
+            
+
+            for (int i = menor; i <= maior; i++)
+            {
+                if (i % 13 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine($"A soma é {soma}");
+
         }
     }
 }
