ded4a02e6315f8a421f5f3473a8d2f0063169d60
Majulia <majulialcarvalho@gmail.com>
2021-10-07 22:10:42 -0300

    finalizado

---
+++ repetitive-exercices-uri/SequenciasCrescentes1146/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenciasCrescentes1146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            while (numero > 0)
+            {
+                string sequence = "";
+                for (int i = 1; i < numero; i++)
+                {
+                    sequence +=  i + " ";
+                }
+                sequence += numero;
+                Console.WriteLine(sequence);
+                numero = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
