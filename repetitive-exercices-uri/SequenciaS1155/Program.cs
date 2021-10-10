ebd01d86dc59e6854f72038ce4ec33f3802cf79f
Majulia <majulialcarvalho@gmail.com>
2021-10-10 14:56:13 -0300

    +1

---
+++ repetitive-exercices-uri/SequenciaS1155/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace SequenciaS1155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double soma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += (1.0 / i);
+            }
+            Console.WriteLine($"A soma foi {soma.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
