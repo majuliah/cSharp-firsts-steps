9f3d1b6871c203ec5db72e67f0565b23a6f7d574
Majulia <majulialcarvalho@gmail.com>
2021-10-10 14:22:13 -0300

    +1

---
+++ repetitive-exercices-uri/FibonacciFacil1151/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FibonacciFacil1151
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número de sequências: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < numero; i++)
+            {
+                if (i == numero - 1)
+                    Console.WriteLine(a);
+                else
+                    Console.Write(a + " ");
+
+                int aux = b;
+                b = a;
+                a = aux + b;
+            }
+        }
+    }
+}
