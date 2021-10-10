6e4d95e89d2f9adf0a03686d28d5c0df9b919d72
Majulia <majulialcarvalho@gmail.com>
2021-10-10 14:36:56 -0300

    + 1

---
+++ repetitive-exercices-uri/Divisores1-1157/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Divisores1_1157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Entre com um número");
+            numero = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
