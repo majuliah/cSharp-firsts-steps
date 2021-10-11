7cc0e2472aaa2669196cce6c3a085a22a48cf972
Majulia <majulialcarvalho@gmail.com>
2021-10-10 22:30:48 -0300

    +1

---
+++ repetitive-exercices-uri/FatorialSimples1153/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FatorialSimples1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, fatorial = 0;
+            Console.WriteLine("Entre com um número!");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                fatorial += numero * (numero - i);
+            }
+            Console.WriteLine($"Fatorial é: {fatorial}");
+        }
+    }
+}
