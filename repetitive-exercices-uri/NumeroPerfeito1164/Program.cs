4be36843acbb05419ed8f3eb62f3e93c7890f11d
Majulia <majulialcarvalho@gmail.com>
2021-10-13 16:33:00 -0300

    +1

---
+++ repetitive-exercices-uri/NumeroPerfeito1164/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NumeroPerfeito1164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada, numero, soma = 0;
+            Console.WriteLine("Entre com a quantidade de verificações:");
+            entrada = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= entrada; i++)
+            {
+                Console.WriteLine("Número:");
+                numero = int.Parse(Console.ReadLine());
+                for (int j = 1; j < numero; j++)
+                {
+                    if (numero % j == 0)
+                    {
+                        soma += j;
+                    }
+                }
+                if (soma == numero)
+                {
+                    Console.WriteLine($"{numero} eh perfeito");
+                }
+                else
+                {
+                    Console.WriteLine($"{numero} não eh perfeito");
+                }
+            }
+        }
+    }
+}
