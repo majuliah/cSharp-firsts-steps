3923bfdafe70226627a8df5c5db25e05dd6b3c37
Majulia <majulialcarvalho@gmail.com>
2021-10-14 20:29:24 -0300

    +1

---
+++ repetitive-exercices-uri/NumeroPrimo1165/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumeroPrimo1165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número de verificações: ");
+            int verificacoes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < verificacoes; i++)
+            {
+                int numero = int.Parse(Console.ReadLine());
+                int metade = numero / 2;
+
+
+                bool primo = true;
+
+                for (int j = 2; j <= metade; j++)
+                {
+                    if (numero / 2 == 0)
+                    {
+                        primo = false;
+                        break;
+                    }
+                }
+
+                if (primo)
+                {
+                    Console.WriteLine("Ehr primo");
+                }
+                else
+                {
+                    Console.WriteLine("Não ehr primo");
+                }
+            }
+        }
+    }
+}
