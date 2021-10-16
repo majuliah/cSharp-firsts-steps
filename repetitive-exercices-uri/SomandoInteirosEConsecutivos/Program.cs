f30e5a1a115bba742f3fa35d12d50d151ceb46a7
Majulia <majulialcarvalho@gmail.com>
2021-10-16 20:57:38 -0300

    +1

---
+++ repetitive-exercices-uri/SomandoInteirosEConsecutivos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SomandoInteirosEConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroA, numeroN, i = 1, soma = 0;
+            Console.WriteLine("Entre com dois valores");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            numeroA = int.Parse(valores[0]);
+            numeroN = int.Parse(valores[i]);
+            while (numeroN <= 0)
+            {
+                i += 1;
+                numeroN = int.Parse(valores[1]);
+            }
+
+            for (int j = 0; j < numeroN; j++)
+            {
+                soma += numeroA + i;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
