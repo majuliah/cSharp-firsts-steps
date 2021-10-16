82b2b37c6930b96d8e3dcab6b1d2e7135544067b
Majulia <majulialcarvalho@gmail.com>
2021-10-16 20:18:53 -0300

    erro de impressão no console

---
+++ repetitive-exercices-uri/SequenciaLogica2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SequenciaLogica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o intervalo");
+            string[] intervalo = Console.ReadLine().Split(' ');
+            int um = int.Parse(intervalo[0]);
+            int dois = int.Parse(intervalo[1]);
+            
+            for (int i = 1; i <= dois; i++)
+            {
+                Console.WriteLine(i);
+                if (i % um == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
