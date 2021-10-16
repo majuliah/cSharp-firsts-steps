25b16526d597cedbeb71451facfae9a68abd55f2
Majulia <majulialcarvalho@gmail.com>
2021-10-16 19:46:23 -0300

    +1

---
+++ repetitive-exercices-uri/SequenciaLogica1144/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SequenciaLogica1144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um valor inteiro");
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                int primeiro = i;
+                int segundo = i * i;
+                int terceiro = i * i * i;
+                Console.WriteLine($"{primeiro} {segundo} {terceiro}");
+                Console.WriteLine($"{primeiro} {segundo + 1} {terceiro + 1}");
+            }
+        }
+    }
+}
