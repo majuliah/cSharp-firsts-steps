5f018703bfcf6341a0aca1310b1951c7977da6c3
Majulia <majulialcarvalho@gmail.com>
2021-10-16 21:17:13 -0300

    penúltimo

---
+++ repetitive-exercices-uri/UltrapassandoZ1150/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UltrapassandoZ1150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1, soma;
+            Console.WriteLine("Entre com dois valores");
+            int valor1 = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine());
+
+            while (valor2 <= valor1)
+            {
+                valor2 = int.Parse(Console.ReadLine());
+            }
+            soma = valor1;
+            do
+            {
+                valor1 += 1;
+                soma += valor1;
+                cont += 1;
+
+            } while (soma <= valor2);
+            Console.WriteLine(cont);
+        }
+    }
+}
