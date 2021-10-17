c39039944a7746acb7b90e7ff12c9a95f20c36bb
Majulia <majulialcarvalho@gmail.com>
2021-10-16 21:10:43 -0300

    +1

---
+++ repetitive-exercices-uri/PUM1142/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PUM1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um valor inteiro");
+            int valor = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= valor; i++)
+            {
+                int segundo = inicio + 1;
+                int terceiro = inicio + 2;
+                Console.WriteLine($"{inicio} {segundo} {terceiro} PUM");
+
+                inicio += 4;
+
+            }
+        }
+    }
+}
