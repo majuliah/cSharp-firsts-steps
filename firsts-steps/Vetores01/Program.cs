eb50c888b4249e949828c124912609f97ec1d851
Majulia <majulialcarvalho@gmail.com>
2021-11-02 10:38:33 -0300

    vetores1

---
+++ firsts-steps/Vetores01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Vetores01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 10, flag = 0, i = 0;
+            double media, soma = 0, numero;
+            double[] info;
+            info = new double[max];
+
+            Console.WriteLine("Entre com os valores numéricos");
+            numero = double.Parse(Console.ReadLine());
+
+            while (numero != flag)
+            {
+                i += 1;
+                info[i] = numero;
+                numero = double.Parse(Console.ReadLine());
+            }
+            numero = i;
+
+            if (numero == max && numero != flag)
+            {
+                Console.WriteLine($"O valor {numero} não foi armazenado porque o vetor está cheio!");
+            }
+
+            if (numero > 0)
+            {
+                for (int j = 0; j < numero; j++)
+                {
+                    soma += info[i];
+                }
+                
+                media = soma / numero;
+
+                for (int j = 0; j < numero; j++)
+                {
+                    Console.WriteLine($"Os valores lidos foram: {info[i]}");
+                }
+
+                Console.WriteLine($"A média aritmética é: {media}");
+            }
+            else
+            {
+                Console.WriteLine("Não há valores para cálculo de média.");
+            }
+        }
+    }
+}
