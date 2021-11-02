94f49d9ead5b9c0396e09f07a63f0c4eefbb0233
Majulia <majulialcarvalho@gmail.com>
2021-11-02 18:39:49 -0300

    Vetores 5

---
+++ firsts-steps/Vetores02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+namespace Vetores02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            string[] valores;
+            double[] vetor;
+            double soma = 0.0, media = 0.0;
+            
+            WriteLine("Entre com a quantidade de posições para o vetor:");
+            index = int.Parse(ReadLine());
+            vetor = new double[index];
+            
+            WriteLine("Entre com os elementos do vetor");
+            valores = ReadLine().Split(' ');
+
+
+            for (int i = 0; i < index; i++)
+            {
+                vetor[i] = double.Parse(valores[i], CultureInfo.InvariantCulture);
+                soma += vetor[i];
+            }
+
+            media = soma / (double)index;
+            
+            WriteLine("Os elementos do vetor são: ");
+            
+            for (int i = 0; i < index; i++)
+                Write(vetor[i] + " ");
+            
+            WriteLine();
+            WriteLine($"A soma é {soma.ToString("F1", CultureInfo.InvariantCulture)} e a média é {media.ToString("F1", CultureInfo.InvariantCulture)}");
+            
+        }
+    }
+}
+
+
---
+++ firsts-steps/Vetores03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using static System.Console;
+
+namespace Vetores03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            string[] nome, informacao;
+            int[] idade;
+            double[] altura;
+            double alturaSoma = 0.0, idadeSoma = 0.0, media = 0.0, porcentagem = 0.0;
+            
+            WriteLine("Entre com o número de posições do array");
+            index = int.Parse(ReadLine());
+            nome = new string[index];
+            idade = new int[index];
+            altura = new double[index];
+            
+            WriteLine("Agora entre com Nome, idade e altura, reespectivamente");
+
+            for (int i = 0; i < index; i++)
+            {
+                informacao = ReadLine().Split(' ');
+                
+                nome[i] = informacao[0];
+                idade[i] = int.Parse(informacao[1]);
+                altura[i] = double.Parse(informacao[2], CultureInfo.InvariantCulture);
+                
+                alturaSoma += altura[i];
+
+                if (idade[i] < 16)
+                    idadeSoma += idade[i];
+                
+            }
+
+            media = alturaSoma / (double) index;
+            porcentagem = idadeSoma / (double) index * 100.0;
+            
+            WriteLine($"A altura média é {media.ToString("F1", CultureInfo.InvariantCulture)}" + "\n" + $" Pessoas com menos de 16 anos: {porcentagem.ToString("F1", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
