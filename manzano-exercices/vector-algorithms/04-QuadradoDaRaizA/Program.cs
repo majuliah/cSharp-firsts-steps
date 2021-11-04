1711e9638a075cbc1fc445119b3a511a06e72ac3
Majulia <majulialcarvalho@gmail.com>
2021-11-04 15:01:54 -0300

    vetores 05

--- manzano-exercices/vector-algorithms/04-QuadradoDaRaizA/Program.cs
+++ manzano-exercices/vector-algorithms/04-QuadradoDaRaizA/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using static System.Console;
 
 namespace _04_QuadradoDaRaizA
 {
@@ -6,7 +8,29 @@ namespace _04_QuadradoDaRaizA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            string[] vetores;
+            
+            WriteLine("Entre com os valores do vetor A: ");
+            vetores = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorA[i] = int.Parse(vetores[i]);
+                vetorB[i] = (int) Math.Pow(vetorA[i], 2);
+            }
+
+            foreach (var number in vetorA)
+                Write(number + " ");
+
+            WriteLine(" ");
+            
+            foreach (var result in vetorB)
+                Write(result + " ");
+
+
+
         }
     }
 }
