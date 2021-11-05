9bda4430b3568ed6a98a6602dee94b0df1edbd69
Majulia <majulialcarvalho@gmail.com>
2021-11-05 15:57:28 -0300

    vetores06

--- manzano-exercices/vector-algorithms/05-FatorialDOVetorA/Program.cs
+++ manzano-exercices/vector-algorithms/05-FatorialDOVetorA/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace _05_FatorialDOVetorA
 {
@@ -6,7 +6,37 @@ namespace _05_FatorialDOVetorA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            string[] vetores;
+            int potencia = 1;
+
+            WriteLine("Entre com os valores para compor o vetor");
+            vetores = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorA[i] = int.Parse(vetores[i]);
+
+                for (int j = vetorA[i]; j > 1; j--)
+                {
+                    potencia *= j;
+                }
+
+                vetorB[i] = potencia;
+                potencia = 1;
+            }
+
+
+            foreach (var number in vetorB)
+            {
+                WriteLine(number);
+            }
+            
+
+            
+                
+
         }
     }
 }
