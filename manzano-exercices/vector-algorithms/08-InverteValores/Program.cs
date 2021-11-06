5f4b7a04057411b45ddb71dfba82e3dbf7a34ef3
Majulia <majulialcarvalho@gmail.com>
2021-11-06 10:30:54 -0300

    08 vetores

---
+++ manzano-exercices/vector-algorithms/08-InverteValores/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace _08_InverteValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Inverte vetor A = vetor B");
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            string[] vetor;
+            int cont = 0;
+            
+            WriteLine("Entre com os valores do vetor A");
+            vetor = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+                vetorA[i] = int.Parse(vetor[i]);
+
+            for (int i = 4; i >= 0; i--)
+            {
+                vetorB[cont] = vetorA[i];
+                cont++;
+            }
+
+            WriteLine("Vetor A ao contrário é: ");
+            
+            foreach (var number in vetorB)
+                Write(number + " ");
+            
+            
+
+        }
+    }
+}
---
+++ manzano-exercices/vector-algorithms/09-JuntarTresVetores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09_JuntarTresVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
