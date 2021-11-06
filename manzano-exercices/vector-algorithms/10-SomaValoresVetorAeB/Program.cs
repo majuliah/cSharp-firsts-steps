cf563be1a08143a147c26fa71197fd1d8769a73e
Majulia <majulialcarvalho@gmail.com>
2021-11-06 15:11:57 -0300

    10-soma

---
+++ manzano-exercices/vector-algorithms/10-SomaValoresVetorAeB/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+namespace _10_SomaValoresVetorAeB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            string[] vetor;
+            int cont = 0;
+            
+            WriteLine("Entre com os valores do vetor A");
+            vetor = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorA[i] = int.Parse(vetor[i]);
+
+                for (int j = vetorA[i]; j >= 1; j--)
+                    cont += j;
+                
+                vetorB[i] = cont;
+                cont = 0;
+            }
+            
+            WriteLine("Vetor B é: ");
+            foreach (var number in vetorB)
+            {
+                Write(number + " ");
+            }
+        }
+    }
+}
---
+++ manzano-exercices/vector-algorithms/11-NegativaDoVetorB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11_NegativaDoVetorB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
