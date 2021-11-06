582f09bd6f157f8256acaea2eea795ae3f229f1f
Majulia <majulialcarvalho@gmail.com>
2021-11-06 15:21:22 -0300

    tudo negativo :)

--- manzano-exercices/vector-algorithms/11-NegativaDoVetorB/Program.cs
+++ manzano-exercices/vector-algorithms/11-NegativaDoVetorB/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace _11_NegativaDoVetorB
 {
@@ -6,7 +6,25 @@ namespace _11_NegativaDoVetorB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] vetor;
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            
+            WriteLine("Entre com os valores do vetor A");
+            vetor = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorA[i] = int.Parse(vetor[i]);
+
+                if (vetorA[i] > 0)
+                    vetorB[i] = vetorA[i] * (-1);
+                else
+                    vetorB[i] = vetorA[i];
+            }
+
+            foreach (var number in vetorB)
+                Write(number + " ");
         }
     }
 }
