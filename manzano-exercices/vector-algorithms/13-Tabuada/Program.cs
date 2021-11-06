faff9ee22b4586163152f403a8a4bf4bc3302163
Majulia <majulialcarvalho@gmail.com>
2021-11-06 19:03:23 -0300

    tabuada

---
+++ manzano-exercices/vector-algorithms/13-Tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace _13_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tabuada = new int[10];
+            int valor;
+            
+            WriteLine("Entre com o número da tabuada");
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+                tabuada[i] = (i + 1) * valor;
+            
+            foreach (var resultado in tabuada)
+                Write(resultado + " ");
+        }
+    }
+}
