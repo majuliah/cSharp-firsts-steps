064b680b3cdda02bfdc1b7c9722192a63a118e18
Majulia <majulialcarvalho@gmail.com>
2021-11-07 18:54:12 -0300

    pronto

--- manzano-exercices/vector-algorithms/26-ImparesEpercentual/Program.cs
+++ manzano-exercices/vector-algorithms/26-ImparesEpercentual/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using static System.Console;
 
 namespace _26_ImparesEpercentual
 {
@@ -6,7 +8,27 @@ namespace _26_ImparesEpercentual
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vetorA = new int[5];
+            string[] vet;
+            int totImpar = 0;
+            double porcentagem;
+
+            for (int i = 0; i < 5; i++)
+            {
+                do
+                {
+                    WriteLine("Entre com valores maior que 0");
+                    vetorA[i] = int.Parse(ReadLine());
+                } while (vetorA[i] < 0);
+
+                if (vetorA[i] % 2 != 0)
+                    totImpar++;
+            }
+
+            porcentagem = (totImpar / 5.0) * 100.0;
+            
+            WriteLine($"O total de números ímpar é: {totImpar} e a porcentagem de números ímpar no vetor é {porcentagem.ToString("F1", CultureInfo.InvariantCulture)}%");
+            
         }
     }
 }
