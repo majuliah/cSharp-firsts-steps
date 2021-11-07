e87fa9708556277490caf07a1329937896461add
Majulia <majulialcarvalho@gmail.com>
2021-11-07 15:45:18 -0300

    26

---
+++ manzano-exercices/vector-algorithms/26-ImparesEpercentual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _26_ImparesEpercentual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
