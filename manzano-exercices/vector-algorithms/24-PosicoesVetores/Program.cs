481c4e68570d6cb20fa7c0405156774f15473fe0
Majulia <majulialcarvalho@gmail.com>
2021-11-07 19:13:31 -0300

    arrumar

--- manzano-exercices/vector-algorithms/24-PosicoesVetores/Program.cs
+++ manzano-exercices/vector-algorithms/24-PosicoesVetores/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace _24_PosicoesVetores
 {
@@ -6,7 +6,42 @@ namespace _24_PosicoesVetores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] vetor;
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            int impar = 1;
+            int par = 0;
+            
+            WriteLine("Entre com os valores do vetor A:");
+            vetor = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorA[i] = int.Parse(vetor[i]);
+                if (i % 2 == 0)
+                {
+                    vetorB[impar] = vetorA[i];
+                    impar += 2;
+                }
+                else
+                {
+                    vetorB[par] = vetorA[i];
+                    par += 2;
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Write($"Posição {i} é {vetorA[i]}, ");
+            }
+            
+            WriteLine(" --- ");
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Write($"Posição {i} é {vetorB[i]}, ");
+            }
+            
         }
     }
 }
