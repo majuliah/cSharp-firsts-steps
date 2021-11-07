dee77c7b23b8b295e1c9947b5fb49963aa8d1ca5
Majulia <majulialcarvalho@gmail.com>
2021-11-07 15:43:25 -0300

    prontinho

---
+++ manzano-exercices/vector-algorithms/25-TotalParesNoVetor/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace _25_TotalParesNoVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[5];
+            string[] vetor;
+            int totPar = 0;
+            
+            WriteLine("Entre com os valores do vetor A:");
+            vetor = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                valores[i] = int.Parse(vetor[i]);
+                if (valores[i] % 2 == 0)
+                    totPar++;
+            }
+            
+            Write($"Existem {totPar} valores par no vetor A.");
+            
+        }
+    }
+}
