c1adf04ff33fb8034313e5bcb97ca9f3eb4f0054
Majulia <majulialcarvalho@gmail.com>
2021-11-07 18:57:07 -0300

    +1

---
+++ manzano-exercices/vector-algorithms/24-PosicoesVetores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24_PosicoesVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
