3475d3a5db22e4b909ab043b9d96bb87d9cd22a1
Majulia <majulialcarvalho@gmail.com>
2021-11-07 11:23:35 -0300

    15 16 prontos

---
+++ manzano-exercices/vector-algorithms/15-ConverteCelciusEmFarenheit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using static System.Console;
+
+namespace _15_ConverteCelciusEmFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetorCelcius = new double[5];
+            double[] vetorFahrenheit = new double[5];
+            string[] graus;
+            
+            WriteLine("Entre com as temperaturas em graus celcius");
+            graus = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorCelcius[i] = double.Parse(graus[i],CultureInfo.InvariantCulture);
+                vetorFahrenheit[i] = (vetorCelcius[i] * 9) / 5 + 32.0;
+            }
+
+            foreach (var grau in vetorFahrenheit)
+                Write(grau.ToString("F1", CultureInfo.InvariantCulture) + " ");
+        }
+    }
+}
---
+++ manzano-exercices/vector-algorithms/16-ImparMultiplicaPor2/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+namespace _16_ImparMultiplicaPor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetorA = new int[5];
+            int[] vetorB = new int[5];
+            string[] valores;
+            
+            WriteLine("Entre com os valores para compor o vetor");
+            valores = ReadLine().Split(' ');
+
+            for (int i = 0; i < 5; i++)
+            {
+                vetorA[i] = int.Parse(valores[i]);
+
+                if (vetorA[i] % 2 != 0)
+                    vetorB[i] = vetorA[i] * 2;
+                else
+                    vetorB[i] = vetorA[i];
+            }
+
+            foreach (var result in vetorB)
+                Write(result + " ");
+        }
+    }
+}
