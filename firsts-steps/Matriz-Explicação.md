221b5d9a0c95db6a75565aeecce27dcd20f62f02
Majulia <majulialcarvalho@gmail.com>
2021-11-08 16:51:24 -0300

    iniciando matrizes

---
+++ firsts-steps/Matriz-Explicação.md
@@ -0,0 +1,38 @@
+## MATRIZ🐱‍🏍
+
+
+
+### Corresponde a uma coleção de dados de tamanho fixo, indexada de tamanho bidimensional. Também chamadas de arranjos bidimensionais, em C# a primeira dimensão de uma matriz é 0,0 (linha 0, coluna 0). Deve ser alocado previamente antes de ser utilizado. 
+
+## Como criar uma matriz?
+
+#### Primeiro tem que ser declarada, assim como no vetor:
+
+#### double[ , ] A;
+
+#### A = new double[3, 4];
+
+#### Acessar os elementos de uma matriz? Temos que informar o índice da linha e o índice da coluna. 
+
+#### A[1, 2] = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ manzano-exercices/matrix-algorithms/01-Primeiro Exemplo/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+namespace _01_Primeiro_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrizA = new int[2, 3];
+            string[] linha;
+            
+            for (int i = 0; i < 2; i++)
+            {
+                linha = ReadLine().Split(' ');
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizA[i, j] = int.Parse(linha[j]);
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Write(matrizA[i, j] + " ");
+                }
+                WriteLine(" ");
+            }
+            
+        }
+    }
+}
