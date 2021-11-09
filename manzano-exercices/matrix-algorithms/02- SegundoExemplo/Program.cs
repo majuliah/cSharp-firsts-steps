7831350bc766e37d2e7fe6a13c9506a13dabe3c9
Majulia <majulialcarvalho@gmail.com>
2021-11-09 17:12:35 -0300

    arrumar

--- manzano-exercices/matrix-algorithms/02- SegundoExemplo/Program.cs
+++ manzano-exercices/matrix-algorithms/02- SegundoExemplo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace _02__SegundoExemplo
 {
@@ -6,8 +6,45 @@ namespace _02__SegundoExemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int.Parse(Console.Title);
+            int[,] matrizA = new int[3,3];
+            int[,] matrizB = new int[3,3];
+            int[,] matrizC = new int[3,3];
+            string[] valores;
+            
+            WriteLine("Entre com os valores da matriz 3x3");
+
+            for (int i = 0; i < 3; i++)
+            {
+                valores = ReadLine().Split(' ');
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizA[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                valores = ReadLine().Split(' ');
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizB[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizC[i, j] = matrizA[i, j] + matrizB[i, j];
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    Write(matrizC[i,j] + ' ');
+            }
+            
         }
     }
 }
