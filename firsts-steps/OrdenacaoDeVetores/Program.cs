dcde41706ea5bcd57b51f774c5575f4cec0e5acc
Majulia <majulialcarvalho@gmail.com>
2021-11-11 09:38:24 -0300

    ordenação de vetores

---
+++ firsts-steps/OrdenacaoDeVetores/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace OrdenacaoDeVetores
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[] vetorLetras = new char[10];
+            string[] letras;
+            char entradas;
+            
+            WriteLine("Entre com 10 letras para efetuarmos a ordenação");
+            letras = ReadLine().Split(' ');
+
+            for (int i = 0; i < 10; i++)
+                vetorLetras[i] = char.Parse(letras[i]);
+            
+            
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (vetorLetras[i] > vetorLetras[j])
+                    {
+                        entradas = vetorLetras[i];
+                        vetorLetras[i] = vetorLetras[j];
+                        vetorLetras[j] = entradas;
+                    }
+                }
+            }
+            for (int i = 0; i < 10; i++)
+                Write(vetorLetras[i] + " ");
+        }
+    }
+}
